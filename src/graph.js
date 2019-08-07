11a614114aa191a440c6d2ca48ffb96464c0eacf
akokai <akokai@berkeley.edu>
2019-08-06 17:21:07 -0700

    Basic framework for node titles and click events

--- src/graph.js
+++ src/graph.js
@@ -14,33 +14,18 @@ var edge;
 
 const simulation = d3
   .forceSimulation()
-  .force(
-    "link",
-    d3.forceLink().id(function(d) {
-      return d.id;
-    })
-  ) // use link.strength() to gather groupings of nodes?
+  .force("link", d3.forceLink().id(d => d.id)) // use link.strength() to gather groupings of nodes?
   .force("charge", d3.forceManyBody())
   .force("center", d3.forceCenter(width / 2, height / 2));
 
 function ticked() {
   edge
-    .attr("x1", function(d) {
-      return d.source.x;
-    })
-    .attr("y1", function(d) {
-      return d.source.y;
-    })
-    .attr("x2", function(d) {
-      return d.target.x;
-    })
-    .attr("y2", function(d) {
-      return d.target.y;
-    });
+    .attr("x1", d => d.source.x)
+    .attr("y1", d => d.source.y)
+    .attr("x2", d => d.target.x)
+    .attr("y2", d => d.target.y);
 
-  node.attr("transform", function(d) {
-    return "translate(" + d.x + ", " + d.y + ")";
-  });
+  node.attr("transform", d => "translate(" + d.x + ", " + d.y + ")");
 }
 
 function dragstarted(d) {
@@ -60,13 +45,24 @@ function dragended(d) {
   d.fy = null;
 }
 
+function nodeClicked(d) {
+  // TODO
+  console.log(d.title);
+}
+
+function edgeClicked(d) {
+  // TODO
+  console.log(d.description);
+}
+
 function visualize(data) {
   edge = svg
     .selectAll(".edge")
     .data(data.edges)
     .enter()
     .append("line")
-    .classed("edge", true);
+    .classed("edge", true)
+    .on("click", edgeClicked);
 
   node = svg
     .selectAll(".node")
@@ -74,6 +70,7 @@ function visualize(data) {
     .enter()
     .append("g")
     .classed("node", true)
+    .on("click", nodeClicked)
     .call(
       d3
         .drag()
@@ -83,9 +80,11 @@ function visualize(data) {
     );
 
   node.append("circle").attr("r", 5);
-  node.append("title").text(function(d) {
-    return d.label;
-  });
+  node
+    .append("text")
+    .text(d => d.label)
+    .attr("text-anchor", "middle")
+    .attr("alignment-baseline", "middle");
 
   simulation.nodes(data.nodes).on("tick", ticked);
 
